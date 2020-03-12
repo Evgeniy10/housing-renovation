5313b0672eb514e0b53a137189ec3a5dae633a4d
Evgeniy10 <vozdux.81@yandex.ru>
2020-03-12 10:35:04 +0500

    add: aquery

--- js/main.js
+++ js/main.js
@@ -1,17 +1,32 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const closeBtn = document.querySelector('.modal__close');
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  }
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
-  });
-  closeBtn.addEventListener('click', switchModal);
-});
-
+// document.addEventListener("DOMContentLoaded", function (event) {
+//   const modal = document.querySelector('.modal');
+//   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+//   const closeBtn = document.querySelector('.modal__close');
+//   const switchModal = () => {
+//     modal.classList.toggle('modal--visible');
+//   }
+//   modalBtn.forEach(element => {
+//     element.addEventListener('click', switchModal);
+//   });
+//   closeBtn.addEventListener('click', switchModal);
+// });
 
 // console.log('скрипт загрузился');
 // console.log(modal);
 // console.log(modalBtn);
+
+
+
+$(document).ready(function () {
+  var modal = $('.modal'),
+    modalBtn = $('[data-toggle=modal]'),
+    closeBtn = $('.modal__close');
+
+
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+});
